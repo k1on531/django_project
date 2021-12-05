e6ba57b2e5c19aff09f288d848dbd393d4873526
rustamakbarov <spearssid@mail.ru>
2021-12-05 21:32:12 +0500

    add forms, static files, rubric pages

--- project/bboard/urls.py
+++ project/bboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('', index),
+    path('add/', BbCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),    # параметризированный маршрут
+    path('', index, name='index'),
 ]
